d09e7cf8310d53b9d9bba27db993a37396e49847
Andrii Sachuk <ansachuk@icloud.com>
2024-12-01 19:30:20 +0100

    base settings and first step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+// import { RouterOutlet } from '@angular/router';
+
+import { TestComponent } from "./components/test/test.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [TestComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'angular-course';
+	title = "interview-master";
 }
